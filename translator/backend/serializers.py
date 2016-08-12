fa6b1012735a06811fabc90d31125b73cfb199d9
Angelika Jarosz <angelikajarosz@gmail.com>
2016-08-12 15:54:23 -0400

    resolves #16

--- translator/backend/serializers.py
+++ translator/backend/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Translation
 
 class TranslationSerializer(serializers.ModelSerializer):
+    language = serializers.CharField(read_only=True)
+    translation = serializers.CharField(read_only=True)
 
     class Meta:
         model = Translation
